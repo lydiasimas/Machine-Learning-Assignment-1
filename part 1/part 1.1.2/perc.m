a9abc08e400b9403a5b871b22eddbe41ca9862c3
Lydia Simantiraki <33206144+lydiasimas@users.noreply.github.com>
2017-11-22 23:55:23 +0100

    batch version

--- part 1/part 1.1.2/perc.m
+++ part 1/part 1.1.2/perc.m
@@ -8,8 +8,7 @@ function y = perc(w, X)
 %   The first argument is the weight vector w and the second argument is a
 %   matrix with input vectors in its columns X. Both use homogeneous
 %   coordinates. The output y is a binary vector with class labels 1 if the
-%   vector is on the positive side of w, -1 if it is on the negative, or 0
-%   if the point lies on the hyperplane.
+%   vector is on the positive side of w or -1 if it is on the negative.
 
 y = w'*X;
 for i = 1:N
--- part 1/part 1.1.2/percTrain.m
+++ part 1/part 1.1.2/percTrain.m
@@ -11,39 +11,36 @@ flag = 1;
 t = t * [1 1 1];
 Xt = X.*t';
 gamma = 1;
-w = zeros(d,1);
+w = zeros(d,1)
 
 if online == true
     % online version
-    count=0;
+    count = 0;
     while flag ~= 0
-        
         flag = 0;
-        
         for i=1:N
-            if j <= 0
+            if w'*Xt(:,i) <= 0
                 flag = 1; % found misclassified vector
                 w = w + gamma*Xt(:,i);     
             end
         end
-        count=count+1;
+        count = count + 1;
     end
     count
 else
     % batch version
-    count = 0;
     
+    count = 0;
     while flag ~= 0 && count < maxIts
         flag = 0;
         count = count + 1;
         Dw = zeros(d,1);
         for i=1:N
             if w'*Xt(:,i) <= 0
-                flag = 1;
+                flag = 1; % found misclassified vector
                 Dw = Dw + Xt(:,i);
             end
         end
         w = w + gamma*Dw;
     end
-    count
 end

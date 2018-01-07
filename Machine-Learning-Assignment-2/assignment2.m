c103149325338e61d95f543a7a6fcda558cf8d74
Jeanny Pan <jeannypan@Jeannys-MacBook-Pro.local>
2018-01-07 22:00:16 +0100

    add rbfKernel
    add rbfKernel

--- Machine-Learning-Assignment-2/assignment2.m
+++ Machine-Learning-Assignment-2/assignment2.m
@@ -17,6 +17,7 @@ end
 
 scatter(train_set(:, 1), train_set(:, 2),[], train_set(:, 3), 'filled');
 hold on;
+
 [alpha, w0] = trainSVM(train_set(:, 1:2), train_set(:,3));
 
 w = sum(alpha.*train_set(:, 3).*train_set(:, 1:2), 1);
@@ -27,3 +28,5 @@ w = - w ./ w(3);
 x = linspace(0, 1, 10);
 y = x*w(2) + w(1);
 plot(x, y);
+
+k=rbfkernel(train_set(:, 1), train_set(:, 2),1);
